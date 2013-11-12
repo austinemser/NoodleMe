7ca3e94364a83fdb3a0565977878ff7e3729698b
Austin Emser <austinemser@Austins-MacBook-Pro.local>
2013-11-11 22:46:29 -0500

    Initial Commit

---
+++ NoodleMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NoodleMe
+//
+//  Created by Austin Emser on 11/11/13.
+//  Copyright (c) 2013 Austin Emser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NoodleMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NoodleMe
+//
+//  Created by Austin Emser on 11/11/13.
+//  Copyright (c) 2013 Austin Emser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
